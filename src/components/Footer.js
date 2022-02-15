49c57cee4695861752dc3236d8533c5a03dbadc5
Ankit2kumar <50981000+Ankit2kumar@users.noreply.github.com>
2022-02-15 11:58:13 +0100

    Updates footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
 			<div className="socialMedia">
 				<InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
 			</div>
-			<p> &copy; 2021 ankitpizza.com</p>
+			<p> &copy; 2022 ankitpizza.com</p>
 		</div>
 	);
 }
